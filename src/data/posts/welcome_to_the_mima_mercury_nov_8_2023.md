aae54bccc001bfdf71e51ce4ec74f94df56332ec
sethvincent <sethvincent@gmail.com>
2023-11-08 17:26:18 -0800

    small style changes

---
+++ src/data/posts/welcome_to_the_mima_mercury_nov_8_2023.md
@@ -0,0 +1,15 @@
+---
+title: Welcome to The Mima Mercury
+promoted: true
+slug: welcome_to_the_mima_mercury_nov_8_2023
+author: Seth Vincent
+author_slug: sethvincent
+created: 1
+updated: 1
+topics: []
+---
+
+I've been daydreaming about creating this website for years, and now it is finally happening.
+
+![A character from Our Flag Means Death saying "Oh my God, it's finally happening"](https://media.giphy.com/media/mUVVVDMknqvcisoe4B/giphy.gif)
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,16 +8,16 @@ const { overview } = data.results
 <a href="/elections/thurston/2023" class="no-underline">
 <section class="not-prose bg-secondary-500 py-8 rounded-md">
     <h1 class="text-lg md:text-xl lg:text-3xl text-center mb-2 text-surface-900 font-extrabold">2023 Thurston County Elections</h1>
-    <p class="text-xs md:text-sm text-center text-surface-700 mt-0 mb-4">As of November 7 at 8 p.m. the unofficial results include:</p>
+    <div class="text-xs md:text-sm text-center text-surface-700 mt-0 mb-4">As of November 7 at 8 p.m. the unofficial results include:</div>
     <div class="grid grid-flow-col mb-8">
         <div class="text-center">
-            <p class="text-surface-700 mb-0">Total Ballots Cast</p>
-            <p class="text-surface-900 font-extrabold text-3xl lg:text-8xl mt-0">{overview.total_ballots}</p>
+            <div class="text-surface-700 mb-0">Total Ballots Cast</div>
+            <div class="text-surface-900 font-extrabold text-3xl lg:text-8xl mt-0">{overview.total_ballots}</div>
         </div>
         <div class="text-center">
-            <p class="text-surface-700 mb-0">Total <b>Blank</b> Ballots</p>
+            <div class="text-surface-700 mb-0">Total <b>Blank</b> Ballots</div>
             <div class="grid grid-flow-col">
-                <p class="text-surface-900 font-extrabold text-3xl lg:text-8xl mt-0">{overview.total_ballots_blank}</p>
+                <div class="text-surface-900 font-extrabold text-3xl lg:text-8xl mt-0">{overview.total_ballots_blank}</div>
             </div>
         </div>
     </div>
