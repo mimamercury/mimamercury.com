8651d9dde92f80a0fff88f8b63d61bd7345f3454
sethvincent <sethvincent@gmail.com>
2023-12-10 12:47:18 -0800

    update dependencies

--- src/routes/newsletter/[newsletter_slug]/+page.svelte
+++ src/routes/newsletter/[newsletter_slug]/+page.svelte
@@ -6,22 +6,22 @@
 
 <svelte:head>
 	<title>{newsletter.title} | The Mima Mercury</title>
-  <meta name="description" content="Recent news about the South Puget Sound">
+    <meta name="description" content="Recent news about the South Puget Sound">
 </svelte:head>
 
 <div class="prose dark:prose-invert">
-<div class="mb-2">
-    <a href="/newsletter" class="not-prose text-xs text-surface-900 bg-secondary-500 border border-secondary-500 font-sans rounded p-1 no-underline hover:shadow">Newsletter</a>
-</div>
-<h1 class="main-header">
-    {newsletter.title}
-</h1>
+    <div class="mb-2">
+        <a href="/newsletter" class="not-prose text-xs text-surface-900 bg-secondary-500 border border-secondary-500 font-sans rounded p-1 no-underline hover:shadow">Newsletter</a>
+    </div>
+    <h1 class="main-header">
+        {newsletter.title}
+    </h1>
 
-<div class="not-prose font-sans text-sm">
-    {format_date(newsletter.updated)} | {newsletter.author}
-</div>
+    <div class="not-prose font-sans text-sm">
+        {format_date(newsletter.updated)} | {newsletter.author}
+    </div>
 
-<div class="main-content">
-    {@html newsletter.content}
-</div>
+    <div class="main-content">
+        {@html newsletter.content}
+    </div>
 </div>
